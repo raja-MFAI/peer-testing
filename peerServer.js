93283876a9699ca94bc8797195886a4d28d92262
RajagopalMFAI <rajagopal.mfai@gmail.com>
2024-10-23 23:37:56 +0530

    old

--- peerServer.js
+++ peerServer.js
@@ -2,7 +2,7 @@ const { PeerServer } = require('peer');
 
 // Create PeerServer on a specific port and domain.
 const peerServer = PeerServer({
-  port: 3001, 
+  port: 443, 
   path: '/peerjs',
   allow_discovery: true,
 });
